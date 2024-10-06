b755fbcaa573bcf1b594d52f35801bc349a24d3d
ahmadnazir2006 <ahmadnazir9101@gmail.com>
2024-10-06 10:58:10 +0500

    Delete 24l0810_2.cpp

--- 24l0810_2.cpp
+++
@@ -1,5 +0,0 @@
-﻿Build started at 11:01 PM...
-1>------ Build started: Project: Project6, Configuration: Debug x64 ------
-1>Project6.vcxproj -> C:\Users\hp\source\repos\Project6\x64\Debug\Project6.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build completed at 11:01 PM and took 00.639 seconds ==========
